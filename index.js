c2045df2292ec3b3401ef8351dc2c81bd484615d
Mitesh Thakor <montu3366@gmal.com>
2017-10-24 14:02:55 +0530

    test mongodb

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+var app = new express();
 var url = "mongodb://imprint:montu123@ds127065.mlab.com:27065/imprint";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+app.get('/', (request, response) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        console.log("Database created!");
+        db.close();
+    });    
 });
